e3d4c53559f3f8ebf9573605ad8931e58c590073
Shawn Tan <shawn@wtf.sg>
2012-01-06 23:12:08 +0800

    Modified code to use RRULE instead

--- cors/pipelines.py
+++ cors/pipelines.py
@@ -4,12 +4,20 @@
 # See: http://doc.scrapy.org/topics/item-pipeline.html
 
 from icalendar import Calendar, Event, vDatetime
-from datetime import datetime, time, date, timedelta
+from datetime import datetime, time, date, timedelta, tzinfo
 import os
+
 def display(cal):
 	return cal.as_string().replace('\r\n','\n').strip()
+class SGT(tzinfo):
+	def utcoffset(self,dt):
+		return timedelta(hours=8)
+	def tzname(self,dt):
+		return "SGT"
+	def dst(self,dt):
+		return timedelta(0)
 
-SEM_START = datetime(2012,1,9,0,0)
+SEM_START = datetime(2012,1,9,0,0,tzinfo=SGT())
 RECESS_WEEK = 7
 TOTAL_WEEKS = 14
 WEEK2ACTUAL=[1,2,3,4,5,6,8,9,10,11,12,13,14]
@@ -18,19 +26,33 @@ day = timedelta(days=1)
 hour = timedelta(hours=1)
 minute = timedelta(minutes=1)
 
+_,startweek,_ = SEM_START.isocalendar()
+
+
+
+
 class CorsPipeline(object):
 	def process_item(self, item, spider):
 		cal = Calendar()
 		cal.add('calscale','GREGORIAN')
 		cal.add('version','2.0')
-		
+
 		if item['exam'] != 'null':
 			exam = Event()
-			exam.add('summary', '%s exam'%item['code'])
-			exam.add('dtstart', datetime.combine(item['exam']['date'], time()))
-			exam.add('dtend', 	datetime.combine(item['exam']['date'], time(12,0)))
+			exam.add('summary', '%s exam %s'%(item['code'],item['exam']['time']))
+			starttime = datetime.combine(
+					item['exam']['date'], time() 
+					if item['exam']['time']=='AM'
+					else time(12,0)
+			).replace(tzinfo=SGT())
+
+			setstartend(
+				exam,
+				starttime,
+				starttime + 12*hour
+			)	
 			cal.add_component(exam)
-			
+
 		if item['lecture_time_table'] != 'null':
 			for l in item['lecture_time_table']:
 				for lec in l['sessions']:
@@ -39,13 +61,19 @@ class CorsPipeline(object):
 					te = int(lec['endtime'])
 					ts_hr,ts_min = ts/100, ts%100
 					te_hr,te_min = te/100, te%100
-					for i in lec['occurence']:
-						lectureday = SEM_START + (dow-1)*day  + (WEEK2ACTUAL[i-1]-1)*week
-						lecture = Event()
-						lecture.add('summary', '%s %s'%(item['code'],l['name']))
-						setstartend(lecture,lectureday + ts_hr*hour + ts_min*minute,lectureday + te_hr*hour + te_min*minute)
-						cal.add_component(lecture)
-						
+					
+					combstr = ','.join([str(WEEK2ACTUAL[i-1]+startweek) for i in lec['occurence']])
+
+					lectureday = SEM_START + (dow-1)*day 
+					lecture = Event()
+					lecture.add('summary', '%s %s'%(item['code'],l['name']))
+					setstartend(lecture,
+							lectureday + ts_hr*hour + ts_min*minute,
+							lectureday + te_hr*hour + te_min*minute)
+					lecture['RRULE'] = 'FREQ=YEARLY;BYWEEKNO=%s;'%combstr
+
+					cal.add_component(lecture)
+
 		if item['tutorial_time_table'] != 'null':
 			for t in item['tutorial_time_table']:
 				for tut in t['sessions']:
@@ -54,17 +82,21 @@ class CorsPipeline(object):
 					te = int(tut['endtime'])
 					ts_hr,ts_min = ts/100, ts%100
 					te_hr,te_min = te/100, te%100
-					for i in tut['occurence']:
-						tutday = SEM_START + (dow-1)*day  + (WEEK2ACTUAL[i-1]-1)*week
-						tutorial = Event()
-						tutorial.add('summary', '%s %s'%(item['code'],t['name']))
-						setstartend(tutorial,tutday + ts_hr*hour + ts_min*minute,tutday + te_hr*hour + te_min*minute)
-						cal.add_component(tutorial)
-						
+
+					combstr = ','.join([str(WEEK2ACTUAL[i-1]+startweek) for i in tut['occurence']])
+
+					tutday = SEM_START + (dow-1)*day
+					tutorial = Event()
+					tutorial.add('summary', '%s %s' % (item['code'],t['name']))
+					setstartend(tutorial,
+							tutday + ts_hr*hour + ts_min*minute,
+							tutday + te_hr*hour + te_min*minute)
+					tutorial['RRULE'] = 'FREQ=YEARLY;BYWEEKNO=%s;'%combstr
+					cal.add_component(tutorial)
+
 		f = open(os.path.join('ics','%s.ics'%item['code']),'wb')
 		f.write(cal.as_string())
 		f.close()
-		
 		return item
 def setstartend(event,start,end):
 	event['dtstart'] = vDatetime(start).ical()
